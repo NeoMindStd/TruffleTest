e0314eb6a36a9590f3e3236e0867c46d547d6293
도우진 <dwj1996@naver.com>
2021-11-24 02:11:22 +0900

    Implement HelloWorld

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.10;
+
+contract HelloWorld {
+    string public str;
+
+    constructor(string memory _str) public {
+        str = _str;
+    }
+
+    function setStr(string memory _str) public {
+        str = _str;
+    }
+
+    function print() public view returns(string memory) {
+        return str;
+    }
+}
